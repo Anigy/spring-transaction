f68e85997a0e83dbbbffd6365ce473530382b7e1
Anigy92 <Anigy92@users.noreply.github.com>
2018-12-23 23:39:09 +0800

    transaction propagation

---
+++ src/com/angiy/spring/tx/CashierImpl.java
@@ -0,0 +1,25 @@
+package com.angiy.spring.tx;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service("cashier")
+public class CashierImpl implements Cashier {
+	
+	@Autowired
+	private BookShopService BookShopService;
+
+	@Transactional()
+	@Override
+	public void checkout(String username, List<String> isbns) {
+		for(String isbn : isbns) {
+			BookShopService.purchase(username, isbn);
+		}
+	}
+	
+	
+	
+}
--- src/com/angiy/spring/tx/SpringTransactionTest.java
+++ src/com/angiy/spring/tx/SpringTransactionTest.java
@@ -2,6 +2,8 @@ package com.angiy.spring.tx;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
@@ -12,6 +14,7 @@ public class SpringTransactionTest {
 	private ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
 	private BookShopDao bookShopDao = ctx.getBean(BookShopDao.class);
 	private BookShopService BookShopService = ctx.getBean(BookShopService.class);
+	private Cashier cashier = ctx.getBean(Cashier.class);
 
 	@Test
 	public void testBookShopDaoFindPriceByIsbn() {
@@ -31,6 +34,11 @@ public class SpringTransactionTest {
 	@Test
 	public void testBookShopService() {
 		BookShopService.purchase("AA", "1001");
+	}
+	
+	@Test
+	public void testTransactionPropagation() {
+		cashier.checkout("AA", Arrays.asList("1001", "1002"));
 	}
 
 }
